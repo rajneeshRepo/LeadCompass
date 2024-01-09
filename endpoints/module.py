8ad99c7420cac5da8ddff688733c1d0e79cb2a75
AnujMaurya <Anuj@shorthills.ai>
2024-01-09 12:51:10 +0530

    Module Flow Done

--- endpoints/module.py
+++ endpoints/module.py
@@ -2,19 +2,22 @@ from datetime import datetime
 
 from bson import ObjectId
 from dotenv import load_dotenv, find_dotenv
-from fastapi import APIRouter, HTTPException, Depends, Body, Query, BackgroundTasks, UploadFile, File
+from fastapi import APIRouter, HTTPException, Depends, Body, Query, BackgroundTasks, UploadFile, File, status
 import subprocess
 from pymongo import MongoClient, UpdateOne
 from pymongo.collection import Collection
+from fastapi.encoders import jsonable_encoder
 import asyncio
+from typing import Optional
 
 from Oauth import get_current_user, create_access_token
 from config.db import get_collection
 from schemas import CreateUserSchema, UserBaseSchema
-from schemas.module import TransactionFilters
+from schemas.module import TransactionFilters, ModuleSchema, ModuleResponse
 from schemas.project import CreateProject
 from utils import hash_password, verify_password, upload_file
 import os
+import traceback
 
 router = APIRouter(
     prefix="",
@@ -46,16 +49,24 @@ def get_project_collection():
     project_collection = db["project"]
     return project_collection
 
+def get_prospects_collection():
+    client = MongoClient("mongodb://localhost:27017")
+    db = client["lead_compass"]
+    prospects_collection = db["prospects"]
+    return prospects_collection
 
-@router.post('/module')
-async def create_module(filters: TransactionFilters, project_id: str = Body(...)):
+
+@router.post('/module', response_model=ModuleResponse, response_model_by_alias=False, response_description="Module added successfully", status_code=status.HTTP_201_CREATED)
+async def create_module(filters: TransactionFilters, project_id: int = Body(...), user: UserBaseSchema = Depends(get_current_user)):
     try:
         print(project_id)
+        print(user)
 
         query_filter = {}
-        if filters.amount:
-            if filters.amount.value == 'All Amounts':
-                query_filter["RcCalTotalLoanAmount"] = {}
+
+        if filters.amount and filters.amount != "All Amounts":
+            # if filters.amount.value == 'All Amounts':
+            #     query_filter["RcCalTotalLoanAmount"] = {}
 
             if filters.amount.value == 'Less than $100,000':
                 query_filter["RcCalTotalLoanAmount"] = {"$lt": 100000}
@@ -63,9 +74,9 @@ async def create_module(filters: TransactionFilters, project_id: str = Body(...)
             if filters.amount.value == 'Greater than $100,000':
                 query_filter["RcCalTotalLoanAmount"] = {"$gt": 100000}
 
-        if filters.transaction_count:
-            if filters.transaction_count.value == 'All Transactions':
-                query_filter["RcCalNumberOfLoans"] = {}
+        if filters.transaction_count and filters.transaction_count != "All Transactions":
+            # if filters.transaction_count.value == 'All Transactions':
+            #     query_filter["RcCalNumberOfLoans"] = {}
 
             if filters.transaction_count.value == 'Greater than 1':
                 query_filter["RcCalNumberOfLoans"] = {"$gt": 1}
@@ -76,9 +87,9 @@ async def create_module(filters: TransactionFilters, project_id: str = Body(...)
             if filters.transaction_count.value == 'Greater than 10':
                 query_filter["RcCalNumberOfLoans"] = {"$gt": 10}
 
-        if filters.duration:
-            if filters.duration.value == 'All Durations':
-                query_filter["RcCalLatestTransactionDate"] = {}
+        if filters.duration and filters.duration != "All Durations":
+            # if filters.duration.value == 'All Durations':
+            #     query_filter["RcCalLatestTransactionDate"] = {}
 
             if filters.transaction_count.value == 'Less than 6 months':
                 query_filter["RcCalLatestTransactionDate"] = {"$gt": 1}
@@ -89,54 +100,131 @@ async def create_module(filters: TransactionFilters, project_id: str = Body(...)
             if filters.transaction_count.value == 'Greater than 1 year':
                 query_filter["RcCalLatestTransactionDate"] = {"$gt": 10}
 
-        collection_mvp_grp = get_group_mvp_collection()
-        filtered_documents = list(collection_mvp_grp.find(query_filter, {'_id': 0}))
+        if filters.states and len(filters.states) != 0:
+            if (filters.county_codes and len(filters.county_codes) > 0):
+                query_filter["$or"] = [{f"FIPSCode.{county_fips}": {
+                    "$exists": True}} for county_fips in filters.county_codes]
+            else:
+                query_filter["$or"] = [{f"PropertyState.{state}": {
+                    "$exists": True}} for state in filters.states]
 
-        new_module = {
-            "project_id": str(project_id),
-            "user_mail": "admin@gmail.com",
+        if filters.borrower_type and filters.borrower_type != "All Borrowers":
+            borrower_type = 'I' if filters.borrower_type == "Individual" else 'C'
+            query_filter[f"RcCalType.{borrower_type}"] = {"$exists": True}
+
+        collection_mvp_grp = get_group_mvp_collection()
+
+        filtered_documents_count = collection_mvp_grp.count_documents(query_filter)
+
+        query_filter["module_id"] = None
+
+        filtered_documents_untag_count = collection_mvp_grp.count_documents(query_filter)
+        
+        if(filtered_documents_untag_count == 0):
+            raise HTTPException(status_code=400, detail="No data found for the given filters")
+        
+        already_taged_documents_count = filtered_documents_count - filtered_documents_untag_count
+        
+        project = get_project_collection().find_one({"project_id": project_id})
+
+        if not project:
+            raise HTTPException(status_code=404, detail="Project not found")
+        
+        project["_id"] = str(project["_id"])
+
+        module_count = get_module_collection().count_documents({"project_id": project["_id"]})
+
+        module_obj = {
+            "name": f"Module {module_count+1}",
+            "status": "bronze",
+            "project_id": project["_id"],
+            "project": project,
+            "user_id": user["_id"],
+            "user_email": user["email"],
+            "filters": jsonable_encoder(filters),
+            "filtered_documents_count": filtered_documents_count,
+            "filtered_documents_untag_count": filtered_documents_untag_count,
+            "already_taged_documents_count": already_taged_documents_count,
+            "timeline": {
+                "raw": {"created_at": project["created_at"]},
+                "bronze": {"created_at": datetime.now()},
+            },
             "created_at": datetime.now(),
-            "filters": query_filter,
+            "updated_at": datetime.now(),
         }
 
-        collection_module = get_module_collection()
-        result_project = collection_module.insert_one(new_module)
+        module_obj = ModuleSchema(**module_obj)
 
-        collection_project = get_project_collection()
+        response = get_module_collection().insert_one(module_obj.model_dump(by_alias=True, exclude=["id"]))
+        
+        collection_mvp_grp.update_many(
+            query_filter, {"$set": {"module_id": response.inserted_id}})
+        
+        filtered_modules = collection_mvp_grp.find({"module_id": response.inserted_id}, {"_id": 0})
 
-        update_result = collection_mvp_grp.update_many(
-            {"RcCalTransactions.ProjectId": int(project_id)},
-            {"$set": {"RcCalTransactions.$.module_id": str(new_module["_id"])}}
-        )
+        # Insert filtered modules into prospects collection
+        get_prospects_collection().insert_many(list(filtered_modules))
+       
+        new_module = get_module_collection().find_one({"_id": response.inserted_id})
 
-        new_module["_id"] = str(new_module["_id"])
-        return {"msg": "module added successfully", "new_module": new_module}
+        return ModuleResponse(result=new_module, message="Module added successfully")
 
     except HTTPException as http_exception:
+        traceback.print_exc()
         raise http_exception
 
     except Exception as e:
+        traceback.print_exc()
         raise HTTPException(status_code=400, detail=str(e))
 
 
-@router.get('/module/all')
+@router.get('/module/all', response_description="List of modules", response_model=ModuleResponse, response_model_by_alias=False, status_code=status.HTTP_200_OK)
 async def get_modules(
-        payload: dict = Body(None, description="source"),
+        project_id: Optional[int] = Query(None),
+        page_size: int = Query(10, ge=1),
         page: int = Query(1, ge=1),
-        page_size: int = Query(100, ge=1)):
+        sort_by: str = Query(None)):
     try:
         collection_module = get_module_collection()
 
-        filter_query = {}
+        project = get_project_collection().find_one({"project_id": project_id})
 
-        modules = collection_module.find(filter_query, {'_id': 0}).sort("created_at", 1).limit(page_size).skip(
-            (page - 1) * page_size)
+        if not project:
+            raise HTTPException(status_code=404, detail="Project not found")
+        
+        sort = -1 if sort_by == "Last entry" else 1
 
-        module_list = [module for module in modules]
-        return {"msg": "modules retrieved successfully", "modules": module_list}
+        filter_query = {"project_id": str(project["_id"])}
+
+        modules = list(collection_module.find(filter_query, {"project": 0, "filters": 0, "timeline": 0}).sort("created_at", sort).limit(page_size).skip((page - 1) * page_size))
+
+        return ModuleResponse(result=modules, total=collection_module.count_documents(filter_query), message="Modules retrieved successfully")
 
     except HTTPException as http_exception:
+        traceback.print_exc()
         raise http_exception
 
     except Exception as e:
+        traceback.print_exc()
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+@router.get('/module/{id}', response_description="Single module", response_model=ModuleResponse, response_model_by_alias=False, status_code=status.HTTP_200_OK)
+async def get_modules(id: str):
+    try:
+        collection_module = get_module_collection()
+
+        module = collection_module.find_one({"_id": ObjectId(id)})
+
+        if not module:
+            raise HTTPException(status_code=404, detail="Module not found")
+
+        return ModuleResponse(result=module, message="Module retrieved successfully")
+
+    except HTTPException as http_exception:
+        traceback.print_exc()
+        raise http_exception
+
+    except Exception as e:
+        traceback.print_exc()
         raise HTTPException(status_code=500, detail=str(e))
---
+++ schemas/prospect.py
@@ -0,0 +1,65 @@
+from typing import List, Optional, Annotated, Union
+from pydantic import BaseModel, Field
+from pydantic.functional_validators import BeforeValidator
+
+PyObjectId = Annotated[str, BeforeValidator(str)]
+
+class Transaction(BaseModel):
+    FIPSCode: int
+    PropertyFullStreetAddress: str
+    PropertyCityName: str
+    PropertyState: str
+    PropertyZipCode: int
+    PropertyZip4: int
+    PropertyUnitType: str
+    PropertyUnitNumber: int
+    APN: Union[str,int] 
+    LenderNameBeneficiary: str
+    LoanAmount: int
+    BuyerMailFullStreetAddress: str
+    OriginalDateOfContract: int
+    DPID: Union[str,int]
+    RcCalPartialLoanAmount: int
+    TransactionId: Union[str,int] 
+    BuyerMailZipCode: int
+    BuyerMailZip4: int
+    BuyerMailUnitType: str
+    BuyerMailUnitNumber: int
+    BuyerMailState: str
+    BuyerMailCity: str
+    RcCalSource: str
+    ProjectId: int
+    RcCalType: str
+
+class ProspectModel(BaseModel):
+    id: Optional[PyObjectId] = Field(alias="_id", default=None)
+    RcCalBorrower: str
+    RcCalTotalLoanAmount: int
+    RcCalNumberOfLoans: int
+    PropertyFullStreetAddress: dict
+    PropertyState: dict
+    PropertyCityName: dict
+    PropertyZipCode: dict
+    PropertyZip4: dict
+    PropertyUnitType: dict
+    PropertyUnitNumber: dict
+    BuyerMailFullStreetAddress: dict
+    BuyerMailCity: dict
+    BuyerMailState: dict
+    BuyerMailZipCode: dict
+    BuyerMailZip4: dict
+    BuyerMailUnitType: dict
+    BuyerMailUnitNumber: dict
+    RcCalType: dict
+    DPID: dict
+    RcCalSource: dict
+    OriginalDateOfContract: dict
+    LenderNameBeneficiary: dict
+    RcCalLatestTransactionDate: int
+    RcCalTransactions: List[Transaction]
+    module_id: Optional[PyObjectId] = Field(alias="module_id", default=None)
+
+class ProspectResponse(BaseModel):
+    message: str
+    total: Optional[int] = None
+    result: List[ProspectModel]
